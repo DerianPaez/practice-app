b362280a311c6271a999ffbdc56f91bc424905dd
Derian Paez <derianpaez01@hotmail.com>
2022-11-08 19:21:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h1>badass</h1>
+    </div>
+  )
+}
+
+export default App
